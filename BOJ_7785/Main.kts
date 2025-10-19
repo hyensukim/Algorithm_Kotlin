00157d6aa1fc355ce99855dbd38998d3eadac031
hyensukim <crazymin17@naver.com>
2025-10-20 00:35:03 +0900

    문제 2

---
+++ BOJ_7785/Main.kts
@@ -0,0 +1,13 @@
+fun main() {
+    val reader = System.`in`.bufferedReader()
+    val n = reader.readLine().toInt()
+
+    val companySet = hashSetOf<String>()
+    repeat(n) {
+        val (company, action) = reader.readLine().split(" ")
+        if(action == "enter") companySet.add(company)
+        else companySet.remove(company)
+    }
+
+    companySet.sortedDescending().joinToString("\n").also{ println(it) }
+}
